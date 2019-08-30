92c772304dcf10e1b20e0849920a7257df3062b4
System Administrator <root@MacBook-Pro-2.local>
2019-08-30 08:02:52 -0300

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ---- ou nao
         </a>
       </header>
     </div>
